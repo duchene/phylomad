f6211c817a8df8165f97209c4bf1c145c0b87fef
duchene <duchenedavid@gmail.com>
2018-03-07 13:22:06 +1100

    completed saturation plots, only pending saturation equation plus thresholds

--- codeFolder/otherScripts/R/test.saturation.R
+++ codeFolder/otherScripts/R/test.saturation.R
@@ -1,24 +1,31 @@
 
-test.saturation <- function(sdata = loci, format = "phylip", para = parallelise, ncore = 1, clean = "cleandata", plotdat = F){
-
-	 runLoc <- function(loc, format = format, aadata = aadata, clean = clean){
+test.saturation <- function(loci, format = "phylip", para = parallelise, ncore = 1, clean = "cleandata", plotdat = F){
+	 aadata <- F
+	 runLoc <- function(loc, format = format, aadata = aadata, clean = clean, plotdat = plotdat){
              if(clean == "cleandata") cleanlogical <- T else cleanlogical <- F
-	     genebin <- clean.gene(sdata = loc, format = format, aadata = F, clean = cleanlogical, plotdat = plotdat)
+	     genebin <- list(clean.gene(sdata = loc, format = format, aadata = F, clean = cleanlogical))
 	     
-	     ## FIGURE OUT CODON POSITION TEST
-	     if(clean == "codonpos")
+	     if(clean == "codonpos") genebin <- list(pos1and2 = genebin[[1]][,c(T,T,F)], pos3 = genebin[[1]][,c(F,F,T)])
 	     
-	     ## FILL IN CODE TO TEST SATURATION!!
+	     satres <- list()
+	     if(plotdat) distdat <- list()
+	     
+	     for(i in 1:length(genebin)){
+	     
+		## FILL IN CODE TO TEST SATURATION!!
 
-	     satres <- "banana"
+	     	satres[[i]] <- "banana"
 	     
-	     ##
+		##
+	     	
+		if(plotdat){
+			dist.raw <- dist.dna(genebin[[i]], model = "raw", pairwise.deletion = T)
+	     		dist.tn93 <- dist.dna(genebin[[i]], model = "TN93", pairwise.deletion = T)
+	     		distdat[[i]] <- list(dist.raw, dist.tn93)
+	     	}
+	     }
 	     
-	     dist.raw <- dist.dna(loc, model = "raw", pairwise.deletion = T)
-	     dist.tn93 <- dist.dna(loc, model = "TN93", pairwise.deletion = T)
-	     distdat <- list(dist.raw, dist.tn93)
-	     
-             tRep <- list(saturation.test.results = satres, genetic.distance.data = distdat)
+             if(plotdat) tRep <- list(saturation.test.results = satres, genetic.distance.data = distdat) else tRep <- list(saturation.test.results = satres)
              return(tRep)
          }
 
@@ -27,24 +34,24 @@ test.saturation <- function(sdata = loci, format = "phylip", para = parallelise,
          if(!para){
 
            for(i in 1:length(loci)){
-	       reslist[[i]] <- runLoc(loci[i], format = format, aadata = F, clean = clean, plotdat = plotdat)
+	       reslist[[i]] <- runLoc(loci[i], format = format, aadata = aadata, clean = clean, plotdat = plotdat)
            }
 
          } else {
+
            ### START PARALLEL COMPUTING
            print('Parallel computing started')
            require(foreach)
            require(doParallel)
            cl <- makeCluster(ncore)
            registerDoParallel(cl)
-           reslist <- foreach(x = 1:length(loci), .packages = c('phangorn', 'ape'), .export = c('format', 'aadata', 'clean', 'clean.gene')) %dopar% runSim(x, format = format, aadata = F, clean = clean, plotdat = plotdat)
+           reslist <- foreach(x = loci, .packages = c('phangorn', 'ape'), .export = c('clean.gene')) %dopar% runLoc(x, format = format, aadata = aadata, clean = clean, plotdat = plotdat)
            stopCluster(cl)
            print("Parallel computing ended successfully")
-          }
-          ### END PARALLEL COMPUTING
+           ### END PARALLEL COMPUTING
+
+	  }
 	  
 	  names(reslist) <- loci
-
 	  return(reslist)
-
 }
---
+++ codeFolder/testBackbones/Saturation/test.saturation.multigene.R
@@ -0,0 +1,71 @@
+source("otherScripts/R/clean.gene.R")
+source("otherScripts/R/test.saturation.R")
+
+
+initial.dir <- getwd()
+
+print("Functions required were loaded successfully")
+
+outs <- list()
+
+locilengths <- vector()
+
+print("Locus was cleaned successfully")
+
+if(input$Ncores > 1) parallelise <- T else parallelise <- F
+
+setwd(input$outputFolder)
+
+print("Output folder was identified successfully")
+
+if(!input$overwrite && (file.exists("saturation.test.phylomad") | file.exists("saturation.plots.pdf") | file.exists("multigene.saturation.plot.pdf"))){
+       stop("Exisitng files will not be overwritten. Aborting.")
+}
+
+whatToOutput <- unlist(input$whatToOutput)
+
+if(input$Ncores > 1) parallelise <- T else parallelise <- F
+
+if("satPlots" %in% whatToOutput | "multiSatPlots" %in% whatToOutput) plotdat <- T else plotdat <- F
+
+geneResults <- test.saturation(loci = as.character(input$dataPath[, 4]), format = input$dataFormat, para = parallelise, ncore = input$Ncores, clean = input$dataTreatment, plotdat = plotdat)
+
+#### Output missing
+
+locinames <- as.character(input$dataPath[, 1])
+
+if("tsat" %in% whatToOutput){
+	
+	#saturation.result <- 
+	#write.csv(saturation.result, file = "saturation.test.phylomad")
+}
+
+if("satPlots" %in% whatToOutput){
+	pdf("saturation.plots.pdf", height = 5, width = 5, useDingbats = F)
+	for(i in 1:length(locinames)){
+		if(input$dataTreatment == "codonpos"){
+			plot(geneResults[[i]][[2]][[2]][[1]] ~ geneResults[[i]][[2]][[2]][[2]], pch = 20, col = "purple", ylab = "Uncorrected pairwise genetic distances", xlab = "Pairwise distances including the ratio\nof transitions to transversions", main = paste0("Saturation plot for\n", locinames[i]))
+			abline(lm(geneResults[[i]][[2]][[2]][[1]] ~ 0 + geneResults[[i]][[2]][[2]][[2]]), lwd=2, col = "purple")
+			points(geneResults[[i]][[2]][[1]][[1]] ~ geneResults[[i]][[2]][[1]][[2]], pch = 20, col = "red")
+			abline(lm(geneResults[[i]][[2]][[1]][[1]] ~ 0 + geneResults[[i]][[2]][[1]][[2]]), lwd=2, col = "red")
+			cor12 <- round(cor.test(geneResults[[i]][[2]][[1]][[1]], geneResults[[i]][[2]][[1]][[2]])$estimate, 3)
+			cor3 <- round(cor.test(geneResults[[i]][[2]][[2]][[1]], geneResults[[i]][[2]][[2]][[2]])$estimate, 3)
+			legend("topleft", legend = c(paste0("1st and 2nd positions, cor = ", cor12), paste0("3rd position, cor = ", cor3)), lty = 1, lwd = 2, col = c("red", "purple"), cex = 0.7)
+		} else {
+			plot(geneResults[[i]][[2]][[1]][[1]] ~ geneResults[[i]][[2]][[1]][[2]], pch = 20, col = "red", ylab = "Uncorrected pairwise genetic distances", xlab = "Pairwise distances including the ratio\nof transitions to transversions", main = paste0("Saturation plot for\n", locinames[i]))
+			abline(lm(geneResults[[i]][[2]][[1]][[1]] ~ 0 + geneResults[[i]][[2]][[1]][[2]]), lwd=2, col = "red")
+			corlocus <- round(cor.test(geneResults[[i]][[2]][[1]][[1]], geneResults[[i]][[2]][[1]][[2]])$estimate, 3)
+			legend("topleft", legend = paste0("cor = ", corlocus), cex = 0.7)
+		}
+		abline(0,1, lty=2)
+	}
+	dev.off()
+}
+
+if("multiSatPlots" %in% whatToOutput){
+	
+}
+
+setwd(initial.dir)
+
+print("Assessment completed successfully.")
